108cf679d877f0df9b3843d411869ebc23c2ba9c
jongkyuh <jongkyuh@naver.com>
2024-07-27 12:37:51 +0900

    12. 회원목록 조회 및 상품 등록
    회원 목록을 조회할 떄 엔티티 내용 그대로를 출력하기 떄문에 엔티티를 그대로 VIEW에 넘겨준다. 그러나 실무에서는 특히 API를 만들떄는 엔티티를 노출시키면 안되기때문에 추후에는 오브젝트 타입에 맞춰서 반환한다. 회원 등록과 마찬가지로 상품등록을 해준다.
    추가적으로 상품등록폼으로 갈때 굳이 상품폼을 같이 가져가는 이유는 컴파일 시점에 미리 에러문구도 확인할 수 있고 폼객체에 맞게 설계 할수 있기떄문에 유지보수에 유리하다.

--- src/main/java/jpabook/jpashoppractice/controller/MemberController.java
+++ src/main/java/jpabook/jpashoppractice/controller/MemberController.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -36,5 +38,13 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";    //첫 페이지로 리다이렉트
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+
     }
 }
